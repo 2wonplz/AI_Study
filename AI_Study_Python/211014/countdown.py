284b4e5fe1f74a6fe7c7227a4ec087bfe4a137bc
2wonplz <92001452+2wonplz@users.noreply.github.com>
2021-10-14 19:39:25 +0900

    Update countdown.py

--- AI_Study_Python/211014/countdown.py
+++ AI_Study_Python/211014/countdown.py
@@ -1,6 +1,6 @@
-# for countdown in 5, 4, 3, 2, 1, "hey!":
-#     print(countdown)
-#     print("들여쓰기로 됩니다.")
+for countdown in 5, 4, 3, 2, 1, "hey!":
+  print(countdown)
+
 
 a = [1, 2, 3, 4, 5]
 print(a[3])
