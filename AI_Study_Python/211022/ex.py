638ff6c2c51c87681b2960b6920fe2c01e358c98
2wonplz <traxrun@gmail.com>
2021-10-25 16:15:39 +0900

    문제 번호 수정

--- AI_Study_Python/211022/ex.py
+++ AI_Study_Python/211022/ex.py
@@ -19,21 +19,21 @@ def bet_sum(n1, n2):
 bet_sum(2, 5)
 bet_sum(1, 5)
 
-# 5.1
+# 5.1.1
 st = [1, 2, 3, 4]
 print(st[0])
 print(st[1])
 print(st[2])
 print(st[3])
 
-# 5.2
+# 5.1.2
 st = [1, 2, 3, 4]
 print(st[0])
 print(st[-3])
 print(st[-2])
 print(st[-1])
 
-# 5.3
+# 5.1.3
 st = [1, 2, 3, 4]
 st[0] += 1
 st[1] += 1
@@ -41,13 +41,13 @@ st[2] += 1
 st[3] += 1
 print(st)
 
-# 5.4
+# 5.1.4
 st = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for i in range(10):
     st[i] += 1
 print(st)
 
-# 5.5
+# 5.1.5
 st = [1, 2, 3, 4, 5, 6]
 st[0], st[5] = st[5], st[0]
 print(st)
