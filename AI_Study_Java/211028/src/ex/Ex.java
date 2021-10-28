1c565cd202896d34f8911736e91e2da077a233a1
2wonplz <traxrun@gmail.com>
2021-10-28 12:33:28 +0900

    211028 Java 수업

---
+++ AI_Study_Java/211028/src/ex/Ex.java
@@ -0,0 +1,12 @@
+package ex;
+
+public class Ex {
+
+	public static void main(String[] args) {
+
+		Circle c = new Circle(2, 2, 4);
+		c.showCircleInfo();
+		
+	}
+
+}
---
+++ AI_Study_Java/211028/src/ex/Point.java
@@ -0,0 +1,20 @@
+package ex;
+
+public class Point {
+	
+	int xPos, yPos;
+	
+	public Point(int x, int y) {
+		
+		xPos = x;
+		yPos = y;
+		
+	}
+	
+	public void showPointInfo() {
+		
+		System.out.println("[" + xPos + ", " + yPos + "]");
+		
+	}
+	
+}
