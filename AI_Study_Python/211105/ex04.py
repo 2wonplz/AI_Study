7ba0cca96d63d7cc69e5044eaacbbf06c7e8626b
2wonplz <traxrun@gmail.com>
2021-11-08 15:01:35 +0900

    함수로 수정

--- AI_Study_Python/211105/ex04.py
+++ AI_Study_Python/211105/ex04.py
@@ -1,11 +1,13 @@
 from openpyxl import load_workbook
 
-wb = load_workbook("c.xlsx")
-ws = wb.active
 
-for x in range(1, 11):
-    for y in range(1, 11):
-        print(ws.cell(x, y).value, end=" ")
-    print()
+def loadCxlsx():
+    wb = load_workbook("c.xlsx")
+    ws = wb.active
 
-wb.close()
+    for x in range(1, 11):
+        for y in range(1, 11):
+            print(ws.cell(x, y).value, end=" ")
+        print()
+
+    wb.close()
