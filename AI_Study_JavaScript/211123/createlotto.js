ea975a271f0f7f79659c7a86b515ba97fd33b74b
2wonplz <traxrun@gmail.com>
2021-11-23 10:33:53 +0900

    211123 Javascript 수업

---
+++ AI_Study_JavaScript/211123/createlotto.js
@@ -0,0 +1,26 @@
+let numbers = []
+let mycount = -1
+let drwnumbers = []
+let lotto_color = ['yellow', 'blue', 'red', 'green', 'gray']
+
+function createLottoNum(drwnum) {
+
+}
+
+function setBackgroundColor(num) {
+
+}
+
+function setFontColor(num) {
+
+}
+
+window.onload = () => {
+    let start_lotto = document.querySelector('#create_lotto')
+    let drwNum = document.querySelector('#drwNum')
+    let all_random = document.querySelector('#all_random')
+    let case_by_case_random = document.querySelector('#case_by_case_random')
+    let same_color = document.querySelector('#same_color')
+    let number_by_number = document.querySelector('#number_by_number')
+    let black_and_white = document.querySelector('#black_and_white')
+}
---
+++ AI_Study_JavaScript/211123/lotto.js
@@ -0,0 +1,12 @@
+function lotto(dwrNo, one, two, three, four, five, six, bns) {
+    this.dwrNo = dwrNo
+    this.one = one
+    this.two = two
+    this.three = three
+    this.four = four
+    this.five = five
+    this.six = six
+    this.bns = bns
+}
+
+let lottos = []
