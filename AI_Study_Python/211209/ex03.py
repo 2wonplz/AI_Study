9eeb281f6b6c47bc7e81e1cf4d697f38d2116326
2wonplz <traxrun@gmail.com>
2021-12-09 15:56:53 +0900

    211209 Python 수업

---
+++ AI_Study_Python/211209/ex03.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+ar1 = np.array([1, 2, 3, 4])
+ar2 = np.arange(4)
+ar3 = np.random.randint(1, 4, (3, 3))
+ar4 = np.zeros((3, 3))
+ar5 = np.ones((3, 3))
+
+print(ar1)
+print(ar2)
+print(ar3)
+print(ar4)
+print(ar5)
---
+++ AI_Study_Python/211209/ex08.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+np1 = np.arange(16).reshape(4, 4)
+
+print(np1)
+print("열합계", np.sum(np1, axis=0))
+print("행합계", np.sum(np1, axis=1))
